ddb2cdb70550c11cae584e052057d4933ad711ac
Matheus <matheuscostadeo@gmail.com>
2024-06-10 11:06:05 -0300

    Update GetTemp.py

--- Python/GetTemp.py
+++ Python/GetTemp.py
@@ -5,12 +5,13 @@ from requests import get
 import json
 
 def dataInsert():
-    latitude = input("insert your latitude: ")
-    longitude = input("insert your longitude: ")
-
-    print("\nlatitude:", latitude)
-    print("longitude:", longitude)
+    latitude = input("Insert your latitude: ")
+    longitude = input("Insert your longitude: ")
+    #print("\nlatitude:", latitude)
+    #print("longitude:", longitude)
+    #get request in API open-meteo.com with your parameters
     getData = get('https://api.open-meteo.com/v1/forecast?latitude='+latitude+'&longitude='+longitude+'&current=temperature_2m&timezone=auto')
+    #verifing if one of the parameters is invaild:
     if not "current" in getData.text:
         print("Request error...\n")
         dataInsert()
@@ -19,11 +20,15 @@ def dataInsert():
     
 def working(getData):
     jsonGet = json.loads(getData.text)
+    #http response:
+    #print(getData.text)
+    #breaking the JSON response:
     currentGet = jsonGet["current"]
     timeGet = currentGet["time"]
     tempGet = currentGet["temperature_2m"]
-    print("\nThe temperature in your region is", tempGet, "°C at", timeGet[11:16])
+    fahrenheit = (float(tempGet)*9/5) + 32
+    #2 decimal numbers for fahrenheit
+    print("\nThe temperature in your region is", tempGet, "°C or",format(fahrenheit, ".2f"),"°F at", timeGet[11:16])
     
     
 dataInsert()
-
